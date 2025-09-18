c468557e741c669220f3ed402564919fc0724d9f
VinhDV <vinhdv@splus-software.com.vn>
2025-09-18 11:29:06 +0700

    refactor docs R6

--- laravel-api/database/migrations/View/2024_08_02_102814_create_admin_policy_view.php
+++ laravel-api/database/migrations/View/2024_08_02_102814_create_admin_policy_view.php
@@ -1,41 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration {
-  /**
-   * Run the migrations.
-   */
-  public function up(): void
-  {
-    DB::statement('
-      CREATE VIEW admin_policy_view AS
-      SELECT
-        am.id          AS admin_id,
-        dm.code        AS department_code,
-        dm.name        AS department_name,
-        pdm.table_name AS table_name,
-        pdm.row_id     AS row_id
-      FROM
-        admin_mst am                                                                -- Account
-        INNER JOIN admin_department_mst adm ON adm.admin_id = am.id                 --
-        INNER JOIN department_mst dm ON dm.id = adm.department_id                   -- Department
-        INNER JOIN department_management_mst dmm ON dmm.department_id = dm.id       --
-        INNER JOIN policy_department_mst pdm ON pdm.id = dmm.policy_department_id   -- Policy
-      WHERE
-        am.status = 1
-        AND dm.status = 1
-    ');
-  }
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement('
+          CREATE VIEW admin_policy_view AS
+          SELECT
+            am.id          AS admin_id,
+            dm.code        AS department_code,
+            dm.name        AS department_name,
+            pdm.table_name AS table_name,
+            pdm.row_id     AS row_id
+          FROM
+            admin_mst am                                                                -- Account
+            INNER JOIN admin_department_mst adm ON adm.admin_id = am.id                 --
+            INNER JOIN department_mst dm ON dm.id = adm.department_id                   -- Department
+            INNER JOIN department_management_mst dmm ON dmm.department_id = dm.id       --
+            INNER JOIN policy_department_mst pdm ON pdm.id = dmm.policy_department_id   -- Policy
+          WHERE
+            am.status = 1
+            AND dm.status = 1
+        ');
+    }
 
-  /**
-   * Reverse the migrations.
-   */
-  public function down(): void
-  {
-    DB::statement("DROP VIEW IF EXISTS admin_policy_view");
-  }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement("DROP VIEW IF EXISTS admin_policy_view");
+    }
 };
